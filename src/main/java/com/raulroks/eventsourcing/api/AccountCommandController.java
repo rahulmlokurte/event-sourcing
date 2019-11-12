bc8d677a8bca3eba9239a7f2c21c0aa0c9d13536
Rahul Lokurte <rahullokurte@Rahuls-MacBook-Air.local>
2019-11-12 21:28:46 +0530

    Added Controller and Swagger

---
+++ src/main/java/com/raulroks/eventsourcing/api/AccountCommandController.java
@@ -0,0 +1,39 @@
+package com.raulroks.eventsourcing.api;
+
+import com.raulroks.eventsourcing.dto.AccountCreateDTO;
+import com.raulroks.eventsourcing.dto.MoneyCreditDTO;
+import com.raulroks.eventsourcing.dto.MoneyDebitDTO;
+import com.raulroks.eventsourcing.services.AccountCommandService;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping(value = "/bank-accounts")
+@Api(value = "Account Commands", description = "Account Commands Related Endpoints", tags = "Account Commands")
+public class AccountCommandController {
+
+    private final AccountCommandService accountCommandService;
+
+    public AccountCommandController(AccountCommandService accountCommandService) {
+        this.accountCommandService = accountCommandService;
+    }
+
+    @PostMapping
+    public CompletableFuture<String> createAccount(@RequestBody AccountCreateDTO accountCreateDTO){
+        return accountCommandService.createAccount(accountCreateDTO);
+    }
+
+    @PutMapping(value = "/credits/{accountNumber}")
+    public CompletableFuture<String> creditMoneyToAccount(@PathVariable(value = "accountNumber") String accountNumber,
+                                                          @RequestBody MoneyCreditDTO moneyCreditDTO){
+        return accountCommandService.creditMoneyToAccount(accountNumber, moneyCreditDTO);
+    }
+
+    @PutMapping(value = "/debits/{accountNumber}")
+    public CompletableFuture<String> debitMoneyFromAccount(@PathVariable(value = "accountNumber") String accountNumber,
+                                                           @RequestBody MoneyDebitDTO moneyDebitDTO){
+        return accountCommandService.debitMoneyFromAccount(accountNumber, moneyDebitDTO);
+    }
+}
