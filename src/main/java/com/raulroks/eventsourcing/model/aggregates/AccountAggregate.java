4f650a0eccf8be6d2f9a8a5787811d8c6c4c9294
Lokurte <Rahulmanikreddy.Lokurte@CenturyLink.com>
2019-11-12 20:10:23 +0530

    Added command Handlers and EventSource Handlers

--- src/main/java/com/raulroks/eventsourcing/model/aggregates/AccountAggregate.java
+++ src/main/java/com/raulroks/eventsourcing/model/aggregates/AccountAggregate.java
@@ -1,6 +1,17 @@
 package com.raulroks.eventsourcing.model.aggregates;
 
+import com.raulroks.eventsourcing.model.commands.CreateAccountCommand;
+import com.raulroks.eventsourcing.model.commands.CreditMoneyCommand;
+import com.raulroks.eventsourcing.model.commands.DebitMoneyCommand;
+import com.raulroks.eventsourcing.model.events.AccountActivatedEvent;
+import com.raulroks.eventsourcing.model.events.AccountCreatedEvent;
+import com.raulroks.eventsourcing.model.events.AccountHeldEvent;
+import com.raulroks.eventsourcing.model.events.MoneyCreditedEvent;
+import com.raulroks.eventsourcing.model.events.MoneyDebitedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @Aggregate
@@ -14,4 +25,62 @@ public class AccountAggregate {
     private String currency;
 
     private String status;
+
+    public AccountAggregate() {
+    }
+
+    @CommandHandler
+    public AccountAggregate(CreateAccountCommand createAccountCommand) {
+        AggregateLifecycle.apply(new AccountCreatedEvent(createAccountCommand.id, createAccountCommand.accountBalance,createAccountCommand.currency));
+    }
+
+    @EventSourcingHandler
+    protected void on(AccountCreatedEvent accountCreatedEvent) {
+        this.id = accountCreatedEvent.id;
+        this.accountBalance = accountCreatedEvent.accountBalance;
+        this.currency = accountCreatedEvent.currency;
+        this.status = String.valueOf(Status.CREATED);
+        AggregateLifecycle.apply(new AccountActivatedEvent(this.id,Status.ACTIVATED));
+    }
+
+    @EventSourcingHandler
+    protected void on(AccountActivatedEvent accountActivatedEvent) {
+        this.status = String.valueOf(accountActivatedEvent.status);
+    }
+
+    @CommandHandler
+    protected void on(CreditMoneyCommand creditMoneyCommand){
+        AggregateLifecycle.apply(new MoneyCreditedEvent(creditMoneyCommand.id, creditMoneyCommand.creditAmount, creditMoneyCommand.currency));
+    }
+
+    @EventSourcingHandler
+    protected void on(MoneyCreditedEvent moneyCreditedEvent){
+
+        if (this.accountBalance < 0 & (this.accountBalance + moneyCreditedEvent.creditAmount) >= 0){
+            AggregateLifecycle.apply(new AccountActivatedEvent(this.id, Status.ACTIVATED));
+        }
+
+        this.accountBalance += moneyCreditedEvent.creditAmount;
+    }
+
+    @CommandHandler
+    protected void on(DebitMoneyCommand debitMoneyCommand){
+        AggregateLifecycle.apply(new MoneyDebitedEvent(debitMoneyCommand.id, debitMoneyCommand.debitAmount, debitMoneyCommand.currency));
+    }
+
+    @EventSourcingHandler
+    protected void on(MoneyDebitedEvent moneyDebitedEvent){
+
+        if (this.accountBalance >= 0 & (this.accountBalance - moneyDebitedEvent.debitAmount) < 0){
+            AggregateLifecycle.apply(new AccountHeldEvent(this.id, Status.HOLD));
+        }
+
+        this.accountBalance -= moneyDebitedEvent.debitAmount;
+
+    }
+
+    @EventSourcingHandler
+    protected void on(AccountHeldEvent accountHeldEvent){
+        this.status = String.valueOf(accountHeldEvent.status);
+    }
 }
